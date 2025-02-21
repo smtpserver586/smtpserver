9b2de0bf2641135056431ea9b18f4f042e381b9c
smtpserver586 <surendarkumar3836@gmail.com>
2025-02-21 08:14:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import secrets
+
+app = Flask(__name__)
+locked = True  # Initial state is locked
+unlock_key = secrets.token_hex(8)  # Generate a random unlock key
+
+def check_unlock_key(key):
+    return key == unlock_key
+
+@app.route('/get-key', methods=['GET'])
+def get_key():
+    return jsonify({"unlock_key": unlock_key})
+
+@app.route('/unlock', methods=['POST'])
+def unlock():
+    global locked
+    data = request.json
+    key = data.get("key", "")
+    if check_unlock_key(key):
+        locked = False
+        return jsonify({"message": "Unlocked successfully!"})
+    else:
+        return jsonify({"message": "Invalid key!"}), 403
+
+@app.route('/locked-status', methods=['GET'])
+def locked_status():
+    return jsonify({"locked": locked})
+
+if __name__ == '__main__':
+    app.run(debug=True)
